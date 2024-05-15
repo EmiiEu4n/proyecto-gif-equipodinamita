b3f24a183379c1a61bf2f7449258e0c0f2540f0c
EmiiEu4n <gaspareuan155@gmail.com>
2024-05-14 18:54:36 -0600

    github page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
+// importamos la biblioteca react
 import React from 'react'
+// importamos la biblioteca reactDom, que proporciona metodos especificos del doom para react
 import ReactDOM from 'react-dom/client'
-import App from './ProyectoGif'
+//importamos un nuevo componente llamado ProyectoGif desde otro archivo en el mismo directorio
+import { ProyectoGif } from './ProyectoGif'
 
+import './styles.css'
+
+// Creamos un nuevo contenedor de raíz React en el elemento con el id 'root' y renderizamos el componente ProyectoGif en el.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ProyectoGif />
   </React.StrictMode>,
 )
